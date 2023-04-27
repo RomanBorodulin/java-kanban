c10dd3dc2614920b2e85f23bc5962eab16d63e1d
RomanBorodulin <djboroda98@yandex.ru>
2023-04-27 13:32:50 +0300

    feat: добавить класс-наследник Subtask, создать конструктор класса, переопределить метод toString()

---
+++ src/model/Subtask.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String name, String description, TaskStatus taskStatus, int epicId) {
+        super(name, description, taskStatus);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", taskStatus=" + taskStatus +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
