1d31fc6daa10c24c283cfb6dc6fbed87b38ee3a2
RomanBorodulin <djboroda98@yandex.ru>
2023-05-02 01:09:24 +0300

    fix: исправить методы getSubtasks(), setTaskStatus()

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -1,17 +1,73 @@
 package model;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Epic extends Task {
-    private HashMap<Integer, Subtask> subtasks = new HashMap<>();
+    private ArrayList<Integer> subtasksId = new ArrayList<>();
 
     public Epic(String name, String description) {
         super(name, description);
 
     }
 
-    public HashMap<Integer, Subtask> getSubtasks() {
-        return subtasks;
+    public ArrayList<Integer> getSubtasks() {
+        return new ArrayList<>(subtasksId);
+    }
+
+    @Override
+    public void setTaskStatus(TaskStatus taskStatus) {
+    }
+
+    // Изменение статусов эпиков
+    public void changeTaskStatus(ArrayList<Subtask> subtasks) {
+        if (subtasks == null) {
+            return;
+        }
+        ArrayList<Integer> listForCheck = new ArrayList<>();
+        for (Subtask subtask : subtasks) {
+            listForCheck.add(subtask.getId());
+        }
+        if (!listForCheck.containsAll(subtasksId)) {
+            return;
+        }
+        if (subtasks.size() == 0) {
+            taskStatus = TaskStatus.NEW;
+            return;
+        }
+        int counterNew = 0;
+        int counterDone = 0;
+        for (Subtask subtask : subtasks) {
+            switch (subtask.getTaskStatus()) {
+                case NEW:
+                    counterNew++;
+                    break;
+                case DONE:
+                    counterDone++;
+                    break;
+                case IN_PROGRESS:
+                    taskStatus = TaskStatus.IN_PROGRESS;
+                    return;
+            }
+        }
+        if (counterNew == subtasks.size()) {
+            taskStatus = TaskStatus.NEW;
+        } else if (counterDone == subtasks.size()) {
+            taskStatus = TaskStatus.DONE;
+        } else {
+            taskStatus = TaskStatus.IN_PROGRESS;
+        }
+    }
+
+    public void clearAllSubtasks() {
+        subtasksId.clear();
+    }
+
+    public void removeSubtaskById(Integer id) {
+        subtasksId.remove(id);
+    }
+
+    public void addSubtask(int id) {
+        subtasksId.add(id);
     }
 
     @Override
@@ -21,7 +77,10 @@ public class Epic extends Task {
                 ", description='" + description + '\'' +
                 ", id=" + id +
                 ", taskStatus=" + taskStatus +
-                ", subtasks.length=" + subtasks.values().size() +
+                ", subtasks.length=" + subtasksId.size() +
                 '}';
     }
+
+
 }
+
