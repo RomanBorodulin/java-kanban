90f504f37552d5c81c7082b9128d6c8004b1d553
RomanBorodulin <djboroda98@yandex.ru>
2023-05-18 23:43:40 +0300

    refactor: убрать метод изменения статусов эпикjd в класс InMemoryTaskManager

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 
 public class Epic extends Task {
-    private ArrayList<Integer> subtasksId = new ArrayList<>();
+    private final ArrayList<Integer> subtasksId = new ArrayList<>();
 
     public Epic(String name, String description) {
         super(name, description);
@@ -12,50 +12,6 @@ public class Epic extends Task {
 
     public ArrayList<Integer> getSubtasks() {
         return new ArrayList<>(subtasksId);
-    }
-
-    @Override
-    public void setTaskStatus(TaskStatus taskStatus) {
-    }
-
-    // Изменение статусов эпиков
-    public void changeTaskStatus(ArrayList<Subtask> subtasks) {
-        if (subtasks == null) {
-            return;
-        }
-        ArrayList<Integer> listForCheck = new ArrayList<>();
-        for (Subtask subtask : subtasks) {
-            listForCheck.add(subtask.getId());
-        }
-        if (!listForCheck.containsAll(subtasksId)) {
-            return;
-        }
-        if (subtasks.size() == 0) {
-            taskStatus = TaskStatus.NEW;
-            return;
-        }
-        int counterNew = 0;
-        int counterDone = 0;
-        for (Subtask subtask : subtasks) {
-            switch (subtask.getTaskStatus()) {
-                case NEW:
-                    counterNew++;
-                    break;
-                case DONE:
-                    counterDone++;
-                    break;
-                case IN_PROGRESS:
-                    taskStatus = TaskStatus.IN_PROGRESS;
-                    return;
-            }
-        }
-        if (counterNew == subtasks.size()) {
-            taskStatus = TaskStatus.NEW;
-        } else if (counterDone == subtasks.size()) {
-            taskStatus = TaskStatus.DONE;
-        } else {
-            taskStatus = TaskStatus.IN_PROGRESS;
-        }
     }
 
     public void clearAllSubtasks() {
