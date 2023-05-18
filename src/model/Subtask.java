bd7cb5d81be37e13f8e0c31ab356acda7fee2416
RomanBorodulin <djboroda98@yandex.ru>
2023-05-18 23:53:40 +0300

    feat: сделать поле final

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Subtask extends Task {
-    private int epicId;
+    private final int epicId;
 
     public Subtask(String name, String description, TaskStatus taskStatus, int epicId) {
         super(name, description, taskStatus);
